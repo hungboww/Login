8b11fc2c5ec34f58511b9fb9f4591376fdc156f3
Your Name <you@example.com>
2022-03-29 15:19:34 +0700

    fix-controoler

--- source/controllers/login.controller.ts
+++ source/controllers/login.controller.ts
@@ -8,7 +8,6 @@ const NAMESPACE = 'User'
 
 const sampleHealthCheck = (req:Request,res:Response, next:NextFunction) =>{
     logging.info(NAMESPACE,`Samples health check route calles`);
-
     return res.status(200).json({
         message:'pong'
     })
@@ -18,54 +17,63 @@ export async function getUser(req:Request,res:Response, next:NextFunction){
     logging.info(NAMESPACE,`Samples health check route calles`);
     let query = 'SELECT * FROM  users';
     Connect()
-        .then(connection =>{
-            Query<IUser>(connection,query)
-                .then(results=>{
+        .then(connection => {
+            Query<IUser>(connection, query)
+                .then(results => {
                     return res.status(200).json({
                         results
                     })
                 })
-                .catch(error=>{
+                .catch(error => {
+                    logging.error(NAMESPACE, error.message, error);
+                    return res.status(500).json({
+                        message: error.message, error
+                    })
+                })
+                // .finally(() => {
+                //         connection.end()}
+                // )
+        })
+         .catch(error=>{
             logging.error(NAMESPACE,error.message,error);
             return res.status(500).json({
                 message:error.message,error})
-
-
         })
-                .finally(() =>{connection.end()}
 
-                )
-        .catch(error=>{
-            logging.error(NAMESPACE,error.message,error);
-            return res.status(500).json({
-                message:error.message,error})
-        })
-})}
+}
+
+
 export function validateToken(req:Request,res:Response, next:Function) {
     logging.info(NAMESPACE,'Token validated')
     return res.status(200).json({
         message:'Authorization'})
 }
+
 export async function registerUser(req:Request,res:Response, next:NextFunction) {
     let {id,name,password,email} = req.body
-
     bcryptjs.hash(password,10,(hashError,hash)=>{
         if (hashError)
         {
             return res.status(500).json({
-                message:hashError.message,error:hashError})
+                message:hashError.message,
+                error:hashError})
         }
-        let query = `INSERT INTO users * VALUES ("${id}","${name}","${hash}","${email}")`;
+        let query = `INSERT INTO users (id,name,password,email) VALUES ("${id}","${name}","${hash}","${email}")`;
         Connect()
-            .then(connection=>{
+            .then((connection)=>{
                 Query<IUser>(connection,query)
                     .then(result=>{
-
+                        return res.status(201).json(result);
                     })
                     .catch((error)=>{
                         return res.status(500).json({message:error.message,error})
                     })
                 })
+             .catch(error=>{
+            logging.error(NAMESPACE,error.message,error);
+            return res.status(500).json({
+                message:error.message,error})
+        })
     })
 }
 // export async function register(req:Request,res:Response, next:Function){
